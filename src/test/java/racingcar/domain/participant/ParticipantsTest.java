732cbb004bd5d607a0d355cb430770584dd907f6
ostar11 <cjw5877@gmail.com>
2025-10-27 02:38:51 +0900

    test(participant): 참가자 생성 및 점수 획득 테스트

---
+++ src/test/java/racingcar/domain/participant/ParticipantsTest.java
@@ -0,0 +1,52 @@
+package racingcar.domain.participant;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.race.RaceRecord;
+import racingcar.domain.race.RaceScore;
+import racingcar.exception.ErrorCode;
+import racingcar.exception.InvalidAttributeException;
+
+class ParticipantsTest {
+
+    @DisplayName("주어진 문자열로부터 참가자 목록 생성")
+    @Test
+    void createParticipantsFromStringArray() {
+        String[] participantNames = new String[]{"a", "ab", "abc", "abcd", "abcde"};
+
+        Participants participants = Participants.from(participantNames);
+
+        RaceRecord raceRecord = participants.getCurrentScores();
+        List<RaceScore> raceScores = raceRecord.getRaceScores();
+        assertThat(raceScores).extracting(RaceScore::getName).contains(participantNames);
+    }
+
+    @DisplayName("주어진 문자열 중 이름이 긴 참가자가 있으면 생성 실패")
+    @Test
+    void longNameParticipantFailCreating() {
+        String[] participantNames = new String[]{"abcde", "abcdef"};
+
+        assertThatThrownBy(() -> Participants.from(participantNames))
+                .isInstanceOf(InvalidAttributeException.class)
+                .hasMessage(ErrorCode.INVALID_CAR_NAME.getMessage());
+    }
+
+    @DisplayName("참가자를 등록하면 0점부터 시작한다.")
+    @Test
+    void ParticipantScoreIsZeroAtStart() {
+        String[] participantNames = new String[]{"a", "ab", "abc"};
+        Participants participants = Participants.from(participantNames);
+
+        RaceRecord raceRecord = participants.getCurrentScores();
+
+        List<RaceScore> raceScores = raceRecord.getRaceScores();
+        assertThat(raceScores).extracting(RaceScore::getName).contains(participantNames);
+        assertThat(raceScores).extracting(RaceScore::getPosition).containsOnly(0);
+    }
+
+}
