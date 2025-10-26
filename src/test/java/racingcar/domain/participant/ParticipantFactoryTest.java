8d45f69326106bc0f07f27e90d071702f5d52c58
ostar11 <cjw5877@gmail.com>
2025-10-27 04:47:34 +0900

    test(particpant): 참가자 생성 및 검증 테스트 작성

---
+++ src/test/java/racingcar/domain/participant/ParticipantFactoryTest.java
@@ -0,0 +1,41 @@
+package racingcar.domain.participant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.race.RaceRecord;
+import racingcar.domain.race.RaceScore;
+import racingcar.exception.InvalidAttributeException;
+
+class ParticipantFactoryTest {
+
+    ParticipantFactory participantFactory = new ParticipantFactory();
+
+    @Test
+    void buildParticipants() {
+        String input = "abc,abcd,abcde";
+
+        Participants participants = participantFactory.build(input);
+
+        RaceRecord raceRecord = participants.getCurrentScores();
+        List<RaceScore> raceScores = raceRecord.getRaceScores();
+        assertThat(raceScores).extracting(RaceScore::getName).containsExactly(input.split(","));
+    }
+
+    @Test
+    void duplicateNameCannotMakeParticipants() {
+        String input = "abc,abcd,abcd";
+
+        assertThatThrownBy(() -> participantFactory.build(input))
+                .isInstanceOf(InvalidAttributeException.class);
+    }
+
+    @Test
+    void wrongNameCannotMakeParticipants() {
+        String input = "abc,abcabc";
+        assertThatThrownBy(() -> participantFactory.build(input))
+                .isInstanceOf(InvalidAttributeException.class);
+    }
+}
---
+++ src/test/java/racingcar/domain/participant/ParticipantValidatorTest.java
@@ -0,0 +1,23 @@
+package racingcar.domain.participant;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.exception.ErrorCode;
+import racingcar.exception.InvalidAttributeException;
+
+class ParticipantValidatorTest {
+
+    static ParticipantValidator participantValidator = new ParticipantValidator();
+
+    @DisplayName("중복된 문자열을 가지면 예외가 발생한다.")
+    @Test
+    void duplicateStringArrayValidate() {
+        String[] strArr = new String[]{"abc", "abc"};
+
+        Assertions.assertThatThrownBy(() -> participantValidator.check(strArr))
+                .isInstanceOf(InvalidAttributeException.class)
+                .hasMessage(ErrorCode.DUPLICATE_CAR_NAME.getMessage());
+    }
+
+}
