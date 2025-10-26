d24e5dc2a55c35535814e393408d4a2e4e94d327
ostar11 <cjw5877@gmail.com>
2025-10-27 02:03:18 +0900

    test(rule): Rule 단위 테스트 작성

---
+++ src/test/java/racingcar/domain/RuleTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RuleTest {
+
+    @DisplayName("처음 만든 rule은 진행중이다.")
+    @Test
+    void createdRuleIsOngoing() {
+        Rule rule = Rule.from(1);
+
+        boolean ongoing = rule.isOngoing();
+
+        assertThat(ongoing).isTrue();
+    }
+
+    @DisplayName("움직이면 rule은 끝난다.")
+    @Test
+    void ruleAfterMovingIsNotOngoing() {
+        int attemptCount = 2;
+        Rule rule = Rule.from(attemptCount);
+        for (int i = 0; i < attemptCount; i++) {
+            rule.nextRound();
+        }
+
+        boolean ongoing = rule.isOngoing();
+
+        assertThat(ongoing).isFalse();
+    }
+}
