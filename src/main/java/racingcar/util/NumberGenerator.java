140c194b4117eef3d83c29af200854bde996a780
ostar11 <cjw5877@gmail.com>
2025-10-27 17:01:21 +0900

    refactor(number): 난수 생성기 숫자 범위를 필드로 변경

--- src/main/java/racingcar/util/NumberGenerator.java
+++ src/main/java/racingcar/util/NumberGenerator.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
 
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
     public static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
