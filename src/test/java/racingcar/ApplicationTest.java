f7fdbb9c136df74e96fb7b2c25fcb6a08842edd7
ostar11 <cjw5877@gmail.com>
2025-10-27 23:29:43 +0900

    test(application): 통합 예외 테스트 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,6 +32,29 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "provideWrongInput")
+    void exceptionTest(String participants, String attemptCount) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(participants, attemptCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    private static Stream<Arguments> provideWrongInput() {
+        return Stream.of(
+                Arguments.of("*,|", "1"),
+                Arguments.of("1,2", "1"),
+                Arguments.of("a , b ,c", "1"),
+                Arguments.of("a,a,b", "1"),
+                Arguments.of("abcdef,a,b", "1"),
+                Arguments.of(",|", "1"),
+                Arguments.of(" ", "1"),
+                Arguments.of("a,b,c", "-1"),
+                Arguments.of("a,b,c", "f")
         );
     }
 
